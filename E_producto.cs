a2ff9f4ffd06eb28ec194ed5ae40cbc6bb485383
RonyM1212 <85724667+RonyM1212@users.noreply.github.com>
2021-07-10 07:59:26 -0600

    CRUD Producto, Capa Entidad

---
+++ E_producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+   public class E_producto
+    {
+        public int cod_producto { get; set; }
+        public string nombre_producto { get; set; }
+        public string descripcion { get; set; }
+        public int precio_actual { get; set; }
+        public int existencia { get; set; }
+        public string marca_producto { get; set; }
+        public int id_proveedor1 { get; set; }
+        public int id_proveedor2 { get; set; }
+    }
+}
