f75919ee31ee08708703ed5908e24f13e5d0d1f9
ermismedina <ermismedina4@gmail.com>
2021-07-17 00:44:32 -0600

    Capa Entidad proveedor

---
+++ E_proveedor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class E_proveedor
+    {
+
+        public int id_proveedor { get; set; }
+        public string rtn_proveedor { get; set; }
+        public string nombre_proveedor { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        }
+}
