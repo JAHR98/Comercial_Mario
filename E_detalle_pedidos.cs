7955ea98ca31ef5198079eb2d1f7233962dd1870
ermismedina <ermismedina4@gmail.com>
2021-07-16 23:56:22 -0600

    Capa Entidad detalle_pedidos Ermis

---
+++ E_detalle_pedidos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class E_detalle_pedidos
+    {
+        public int id_pedido { get; set; }
+        public string id_producto { get; set; }
+        public string fecha_pedido { get; set; }
+        public int fecha_entrega { get; set; }
+        public string precio_compre { get; set; }
+        public string cantidad { get; set; }
+        public string estado { get; set; }
+    }
+}
