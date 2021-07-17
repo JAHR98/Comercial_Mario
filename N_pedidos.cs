26623a88bbddd0a80b44f3b203bf7000ea26ee99
ermismedina <ermismedina4@gmail.com>
2021-07-17 00:33:04 -0600

    Capa negocio pedidos Ermis

---
+++ N_pedidos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+using CapaDatos;
+using CapaEntidades;
+
+namespace CapaNegocios
+{
+    public class N_pedidos
+    {
+        readonly D_pedidos Objpedidos = new D_pedidos();
+
+        public static DataTable MostrarRegistros()
+        {
+            return new D_pedidos().MostrarRegistros();
+        }
+
+        public static DataTable BuscarRegistros(string textobuscar)
+        {
+            return new D_pedidos().BuscarRegistros(textobuscar);
+        }
+        public void InsertarRegistros(E_pedidos pedidos)
+        {
+            Objpedidos.InsertarRegistros(pedidos);
+        }
+
+        public void EditarRegistros(E_pedidos pedidos)
+        {
+            Objpedidos.EditarRegistros(pedidos);
+        }
+        public void EliminarRegistros(E_pedidos pedidos)
+        {
+            Objpedidos.EliminarRegistros(pedidos);
+        }
+    }
+}
