e614c9fe794be23b33c0484110ff945b5acfed5f
ermismedina <ermismedina4@gmail.com>
2021-07-16 23:59:14 -0600

    Capa Entidad pedidos Ermis

---
+++ E_pedidos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class E_pedidos
+    {
+        public int id_pedido { get; set; }
+        public string id_proveedor { get; set; }
+        public string cod_empleado { get; set; }
+    }
+}
