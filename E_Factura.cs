516309eb4a5cb68253fd85c4411d6377ebbaf711
Andrea Garcia <garcianicol96@gmail.com>
2021-07-22 18:06:13 -0400

    Capa Factura Andrea

---
+++ E_Factura.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class E_factura
+    {
+
+        public int No_factura { get; set; }
+        public string fecha { get; set; }
+
+        public int cod_termino{ get; set; }
+   
+        public string id_cliente { get; set; }
+        public int cod_empleado { get; set; }
+
+        public string id_producto { get; set; }
+
+        public int cantidad { get; set; }
+        public string precio_venta { get; set; }
+
+        public int precio_unitario { get; set; }
+
+        public string importe { get; set; }
+        public int ISV { get; set; }
+
+        public string subtotal { get; set; }
+        public int total { get; set; }
+    }
+}
