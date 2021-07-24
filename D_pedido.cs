f8a4ff389c607e26448e6806ecbbee9bfde17454
JohanU22 <johanurborlando2217@gmail.com>
2021-07-24 02:37:10 -0600

    Se adjunta la Capa de Datos de Pedidos

---
+++ D_pedido.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidades;
+
+namespace CapaDatos
+{
+    public class D_pedido
+    {
+        readonly SqlConnection conectar = new SqlConnection(ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString);
+
+        public DataTable MostrarRegistros()
+        {
+            DataTable DtResultado = new DataTable();
+            SqlCommand SqlCmd = new SqlCommand("mostrar_pedido", conectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+            SqlDat.Fill(DtResultado);
+
+            return DtResultado;
+        }
+        public DataTable BuscarRegistros(string textobuscar)
+        {
+            DataTable DtResultado = new DataTable();
+            SqlCommand SqlCmd = new SqlCommand("buscar_pedido", conectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            SqlCmd.Parameters.AddWithValue("@textobuscar", textobuscar);
+
+            SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+            SqlDat.Fill(DtResultado);
+
+            return DtResultado;
+        }
+
+        public void InsertarRegistros(E_pedido pedido)
+        {
+            SqlCommand SqlCmd = new SqlCommand("insertar_pedido", conectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            conectar.Open();
+
+            SqlCmd.Parameters.AddWithValue("@id_proveedor", pedido.id_pedido);
+            SqlCmd.Parameters.AddWithValue("@cod_empleado", pedido.cod_empleado);
+
+
+            SqlCmd.ExecuteNonQuery();
+
+            conectar.Close();
+        }
+        public void EditarRegistros(E_pedido pedido)
+        {
+
+            SqlCommand SqlCmd = new SqlCommand("editar_pedidos", conectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            conectar.Open();
+
+            SqlCmd.Parameters.AddWithValue("@id_pedido", pedido.id_pedido);
+            SqlCmd.Parameters.AddWithValue("@id_proveedor", pedido.id_proveedor);
+            SqlCmd.Parameters.AddWithValue("@cod_empleado", pedido.cod_empleado);
+
+
+            SqlCmd.ExecuteNonQuery();
+
+            conectar.Close();
+        }
+        public void EliminarRegistros(E_pedido pedido)
+        {
+            SqlCommand SqlCmd = new SqlCommand("eliminar_pedido", conectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            conectar.Open();
+            SqlCmd.Parameters.AddWithValue("@id_pedido", pedido.id_pedido);
+
+            SqlCmd.ExecuteNonQuery();
+
+            conectar.Close();
+        }
+    }
+}
